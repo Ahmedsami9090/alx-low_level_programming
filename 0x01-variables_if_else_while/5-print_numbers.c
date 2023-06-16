e240eeef6525662475f6606acffdbea0890e2242
Ahmed Sami <ahmadsami990990@gmail.com>
2023-06-16 14:43:08 -0700

    hgf

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a program
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+
+	return (0);
+}
