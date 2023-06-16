085558b28bd3d4be14e7a4705af993eefb7c24e3
Ahmed Sami <ahmadsami990990@gmail.com>
2023-06-16 15:01:50 -0700

    hgf

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a program for
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+}
