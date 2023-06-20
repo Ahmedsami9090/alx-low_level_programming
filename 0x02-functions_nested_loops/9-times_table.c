03d03e1a5b47f9bf6534ec0b4d2eec4214c4b3ef
Ahmed Sami <ahmadsami990990@gmail.com>
2023-06-20 14:10:31 -0700

    hgf

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * times_table - a function that print
+ *
+ * Return: times table
+*/
+
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(i * j);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
