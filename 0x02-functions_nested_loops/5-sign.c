8b483f327946d9149e87ac53022e9e43ddf98f4b
Ahmed Sami <ahmadsami990990@gmail.com>
2023-06-20 12:58:00 -0700

    hgf

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - a function that print a sign
+ *
+ * @n: function input
+ *
+ * Return: 1 or 0
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43)
+			return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45)
+			return (0);
+	}
+	else
+	{
+		_putchar(0);
+			return (0);
+	}
+}
