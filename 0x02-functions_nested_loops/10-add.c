46c5e91d21bf5e59bcffd17dec4f14a24a2fc046
Ahmed Sami <ahmadsami990990@gmail.com>
2023-06-20 14:55:43 -0700

    hgf

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ *
+ * @a: first input
+ *
+ * @b: sec input
+ *
+ * Return: sum of 2 numbers
+*/
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+}
