6b48c8efb2516cefdecf617eb640289d5e73c0ad
Ahmed Sami <ahmadsami990990@gmail.com>
2023-06-20 15:32:26 -0700

    hgf

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,20 +15,17 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		for (i = n; i >= 99; i--)
+		for (i = n; i > 98; i--)
 		{
 			printf("%d, ", i);
 		}
-		printf("98 ");
-	}
-	else if (n < 98)
-	{
-		for (i = n; i <= 97; i++)
-		{
-			printf("%d, ", i);
-		}
-		printf("98 ");
 	}
 	else
-		printf("98 ");
+	{
+		for (i = n; i < 98; i++)
+		{
+			printf("%d, ", i);
+		}
+	}
+	printf("98\n";
 }
