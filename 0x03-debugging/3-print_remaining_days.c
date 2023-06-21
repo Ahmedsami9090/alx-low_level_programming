25b3f74ffe7f38daea9a72ce564df35062575f21
Ahmed Sami <ahmadsami990990@gmail.com>
2023-06-21 14:44:20 -0700

    lll

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -18,7 +18,7 @@ void print_remaining_days(int month, int day, int year)
 	 * by 100 and 400 or by 4 enently
 	*/
 
-	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	if ((year % 400 == 0 && year % 100 == 0) || (year % 4 == 0))
 	{
 		if (month >= 2 && day >= 60)
 		{
