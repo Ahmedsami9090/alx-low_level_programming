92649de45993e18aaaf41497b91776faac279c91
Ahmed Sami <ahmadsami990990@gmail.com>
2023-06-21 04:24:02 -0700

    hgf

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * @i: function input
+ *
+ * Return: 0 success
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	postitive_or_negative(0);
+
+	return (0);
+}
