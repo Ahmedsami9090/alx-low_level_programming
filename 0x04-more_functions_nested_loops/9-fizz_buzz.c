644d27b3438038620e71aea1920f445595d78800
Ahmed Sami <ahmadsami990990@gmail.com>
2023-06-22 15:49:38 -0700

    hhg

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,22 +16,25 @@ for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
 }
 else if (i % 5 == 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
 else if (i % 3 == 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 else
 {
-printf("%d ", i);
+printf("%d", i);
 }
 }
-printf("\n");
+if (i != 100)
+	printf(" ");
+else
+	printf("\n");
 
 return (0);
 }
