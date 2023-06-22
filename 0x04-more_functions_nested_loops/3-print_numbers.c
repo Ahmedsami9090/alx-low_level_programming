edcd9f6aa3de389f0c9614642d9e25443e03f383
Ahmed Sami <ahmadsami990990@gmail.com>
2023-06-22 12:38:27 -0700

    hhg

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - main
+ *
+ * Description: a function that prints something
+ *
+ * Return: whatever
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
