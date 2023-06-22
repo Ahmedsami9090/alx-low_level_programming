59aef02be1b55d6c25479a57bf5eb9c587db2607
Ahmed Sami <ahmadsami990990@gmail.com>
2023-06-22 14:11:01 -0700

    hhg

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,20 +10,22 @@
 
 void more_numbers(void)
 {
-	int i;
-	int ch;
-	int ch2;
+	int num;
+	int r;
+	int c;
 
-	for (i = 0; i <= 9; i++)
+	for (r = 0; r <= 9; r++)
 	{
-		for (ch = 48; ch <= 57; ch++)
+		for (c = 0; c <= 14; c++)
 		{
-			_putchar(ch);
-		}
-		for (ch2 = 48; ch2 <= 52; ch2++)
-		{
-			_putchar(49);
-			_putchar(ch2);
+			num = c;
+			if ( c > 9)
+			{
+				_putchar('1' + 48);
+				num = c % 10;
+			}
+			_putchar(num + 48);
+
 		}
 		_putchar('\n');
 	}
