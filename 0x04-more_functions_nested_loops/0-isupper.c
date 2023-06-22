12c8a7245e3dfcd944d87feb5af7e1f853c42a79
Ahmed Sami <ahmadsami990990@gmail.com>
2023-06-22 08:50:39 -0700

    ggg

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
-include "main.h
+#include "main.h"
 
 /**
  * _isupper - main entry
  *
- * @c: function input
+ * @c: funtion input
  *
  * Return: 0 or 1
 */
@@ -12,6 +12,6 @@ int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 		_putchar('1');
-	if (c >= 97 && c<= 122)
+	if (c >= 97 && c <= 122)
 		_putchar('0');
 }
