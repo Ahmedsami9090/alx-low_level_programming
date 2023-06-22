6bc86c5c6ebc2e5c9a5774cb1df0308c91f311c6
Ahmed Sami <ahmadsami990990@gmail.com>
2023-06-22 12:03:12 -0700

    jhg

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,15 +3,18 @@
 /**
  * _isupper - main entry
  *
- * @c: funtion input
+ * @c: function input
  *
  * Return: 0 or 1
 */
 
 int _isupper(int c)
 {
+	int retVal;
+
 	if (c >= 65 && c <= 90)
-		_putchar('1');
+		retVal = 1;
 	if (c >= 97 && c <= 122)
-		_putchar('0');
-}
+		retVal = 0;
+
+	return (retVal);
