5cdc7fc37abbf111b0e2224c2c6d9451d3fa509a
Ahmed Sami <ahmadsami990990@gmail.com>
2023-06-26 17:40:51 -0700

    hgf

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+# include "main.h"
+
+/**
+ * print_rev - main entry
+ *
+ * @s: input
+ *
+ * Return: rev string
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while(i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
