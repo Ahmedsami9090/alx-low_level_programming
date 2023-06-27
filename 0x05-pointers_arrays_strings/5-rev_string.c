c95c9b11c98198e3bc41b2250ef38fb6157cef87
Ahmed Sami <ahmadsami990990@gmail.com>
2023-06-26 17:52:34 -0700

    hgf

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+# include "main.h"
+
+/**
+ * rev_string - main entry
+ *
+ * @s: function input
+ *
+ * Return: rev string
+*/
+
+void rev_string(char *s)
+{
+	int j, k;
+	char tmp;
+
+	for (j = 0; s[j] != '\0'; ++j)
+		;
+
+	for (k = 0; k < 1 / 2; i++)
+	{
+		tmp = s[k];
+		s[k] = s[j - 1 - k];
+		s[j - 1 - k} = tmp;
+	}
+}
