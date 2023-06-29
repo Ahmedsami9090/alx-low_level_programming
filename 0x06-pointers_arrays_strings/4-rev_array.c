874af0006eb284c5fe7be4b34d94ded54c1e1530
Ahmed Sami <ahmadsami990990@gmail.com>
2023-06-29 15:50:15 -0700

    hhh

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+# include "main.h"
+
+/**
+ * reverse_array - entry
+ *
+ * @a: string array
+ *
+ * @n: no of elements
+ *
+ * Return: reversed array
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	while (i != 0)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+	for (i = n - 1; i >= 0; i--)
+	{
+		a[i];
+	}
+}
