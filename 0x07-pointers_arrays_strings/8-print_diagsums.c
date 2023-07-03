b897c356db02ad9c4daf0fd709d7d6f6c249ee0d
Ahmed Sami <ahmadsami990990@gmail.com>
2023-07-03 09:34:11 -0700

    jkl

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -14,17 +14,14 @@
 void print_diagsums(int *a, int size)
 {
 	int sum1, sum2;
-	int i, j;
+	int i;
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
-		{
-			if (i == j)
-				sum1 = sum1 + a[i][j];
-			if (i + j == (size - 1))
-				sum2 = sum2 + a[i][j];
-		}
+		sum1 = sum1 + a[i];
+		sum2 = sum2 + a[size - i - 1];
+		a = a + size;
 	}
-	printf("%d, %d", sum1, sum2);
+	printf("%d, ", sum1);
+	printf("%d\n", sum2);
 }
