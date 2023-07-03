84810b9914486b8eb94b03c3473a05682f3913de
Ahmed Sami <ahmadsami990990@gmail.com>
2023-07-03 09:36:16 -0700

    jkl

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -20,7 +20,7 @@ void print_diagsums(int *a, int size)
 	{
 		sum1 = sum1 + a[i];
 		sum2 = sum2 + a[size - i - 1];
-		a = a + size;
+		a += size;
 	}
 	printf("%d, ", sum1);
 	printf("%d\n", sum2);
