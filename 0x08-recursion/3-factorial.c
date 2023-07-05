b89e6b677e0e2300081bd37a6c504c891a4aeffb
Ahmed Sami <ahmadsami990990@gmail.com>
2023-07-05 13:15:08 -0700

    hgf

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,17 +10,14 @@
 
 int factorial(int n)
 {
-	int res = 0;
 
 	if (n < 0)
 	{
-		res = -1;
-		break;
+		return  (-1);
 	}
 	if (n == 0)
 	{
 		return (1);
 	}
-	res = n * factorial(n - 1);
-	return (res);
+	return (n * factorial(n - 1));
 }
