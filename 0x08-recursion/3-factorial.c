7e5cf6af684ee4ecef882de22220f00f6cb1bcb1
Ahmed Sami <ahmadsami990990@gmail.com>
2023-07-05 13:03:39 -0700

    hgf

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,28 @@
+# include "main.h"
+
+/**
+ * factorial - entry point
+ *
+ * @n: input
+ *
+ * Return: result
+*/
+
+int factorial(int n)
+{
+	int res = 0;
+
+	if (n < 0)
+	{
+		res = -1;
+	}
+	if (n == 1)
+	{
+		factorial(n) == 1;
+	}
+	else
+	{
+		res = n * factorial(n - 1);
+	}
+	return (res);
+}
