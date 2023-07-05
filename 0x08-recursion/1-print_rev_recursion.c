0dda8ac39f5d159646537b6980b6f74298012e8c
Ahmed Sami <ahmadsami990990@gmail.com>
2023-07-05 12:32:19 -0700

    hgf

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+# include "main.h"
+
+/**
+ * _print_rev_recursion - entry point
+ *
+ * @s: string
+ *
+ * Return: 0 success
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
