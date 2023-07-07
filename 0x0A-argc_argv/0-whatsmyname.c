fba3be0d3324e0176cc8d52a3f084ee84404e237
Ahmed Sami <ahmadsami990990@gmail.com>
2023-07-07 14:16:30 -0700

    hhh

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: argument counter
+ *
+ * @argv: argument value
+ *
+ * Return: 0 success
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
