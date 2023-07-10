f83386ae8287b09fb6e6820445c776186b6789a7
Ahmed Sami <ahmadsami990990@gmail.com>
2023-07-10 10:09:51 -0700

    kkk

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+# include "main.h"
+# include <stdio.h>
+# include <stdlib.h>
+
+/**
+ * *create_array - entry point
+ *
+ * @size: size of bytes reserved
+ *
+ * @c: first char of array
+ *
+ * Return: char ar
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+
+	ar = malloc(sizeof(char) * size);
+	ar[0] = c;
+
+	return (ar);
+}
