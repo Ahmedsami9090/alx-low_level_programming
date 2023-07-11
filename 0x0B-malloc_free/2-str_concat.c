d7e1c3b12dc5a9df86643ae529718708397f24df
Ahmed Sami <ahmadsami990990@gmail.com>
2023-07-11 13:31:13 -0700

    hgf

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -43,12 +43,12 @@ char *str_concat(char *s1, char *s2)
 	m = malloc((size1 + size2) * sizeof(char) + 1);
 	if (m == 0)
 		return (0);
-	for (i = 0; i < (size1 + size2); i++)
+	for (i = 0; i <= (size1 + size2); i++)
 	{
 		if (i < size1)
 			m[i] = s1[i];
 		else
-			m[i] = s2[i - size2];
+			m[i] = s2[i - size1];
 	}
 	m[i] = '\0';
 	return (m);
