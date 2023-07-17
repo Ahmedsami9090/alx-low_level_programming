d3c44c3c95074cbaa075983f78b4fffa273a4f9a
Ahmed Sami <ahmadsami990990@gmail.com>
2023-07-17 08:52:01 -0700

    hgf

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+# ifndef DOG_H
+# define DOG_H
+
+/**
+ * struct dog - entry point
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: struct dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+# endif
