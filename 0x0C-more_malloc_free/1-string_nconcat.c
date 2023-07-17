555ac98636ddbc37274d6775301c9dcb61123eac
Ahmed Sami <ahmadsami990990@gmail.com>
2023-07-17 08:24:28 -0700

    hgf

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,9 +18,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, len_s1, len_s2;
 
 	if (s1 == NULL)
-		s1 == "";
+		s1 = "";
 	if (s2 == NULL)
-		s2 == "";
+		s2 = "";
 	for (len_s1 = 0; s1[len_s1] != '\0'; len_s1++)
 		;
 	for (len_s2 = 0; s2[len_s2] != '\0'; len_s2++)
