dc60038d23ce0b325a967c1c9e062987278ce420
Ahmed Sami <ahmadsami990990@gmail.com>
2023-07-19 09:55:57 -0700

    hgf

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+
+/**
+ * main - entry point
+ * @argc: input
+ * @argv: input
+ * Return: output
+*/
+
+int main(int argc, char **argv)
+{
+	int (*op_f)(int, int), a, b;
+
+	if (argc != 4)
+		printf("error\n"), exit(98);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	op_f = get_op_func(argv[2]);
+	if (!op_f)
+		printf("error\n"), exit(99);
+	if (!b && ((argv[2] == '/' || argv[2] == '%') && b == 0))
+		printf("error\n"), exit(100);
+	printf("%d\n", op_f(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - entry point
+ * @a: input
+ * @b: input
+ * Return: sum of inputs
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - entry point
+ * @a: input
+ * @b: input
+ * Return: sub of inputs
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - entry point
+ * @a: input
+ * @b: input
+ * Return: mul of inputs
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - entry point
+ * @a: input
+ * @b: input
+ * Return: div of inputs
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - entry point
+ * @a: input
+ * @b: input
+ * Return: mod of inputs
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
