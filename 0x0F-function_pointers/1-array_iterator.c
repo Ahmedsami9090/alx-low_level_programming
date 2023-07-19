8bdcb472924145bb4d6e5bef35665d541ed85327
Ahmed Sami <ahmadsami990990@gmail.com>
2023-07-19 07:24:33 -0700

    hgf

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int *l = array + size - 1;
 
-	if (array && siae && action)
+	if (array && size && action)
 		while (array <= l)
 			action(*array++);
 }
