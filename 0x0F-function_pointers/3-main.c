6aed1ad15f1191aca11b4ddef3fedee2dfec2b74
Ahmed Sami <ahmadsami990990@gmail.com>
2023-07-19 11:15:58 -0700

    hgf

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,16 +12,16 @@ int main(int argc, char **argv)
 	int (*op_f)(int, int), a, b;
 
 	if (argc != 4)
-		printf("error\n"), exit(98);
+		printf("Error\n"), exit(98);
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
 	op_f = get_op_func(argv[2]);
 	if (!op_f)
-		printf("error\n"), exit(99);
+		printf("Error\n"), exit(99);
 	if (!b && ((*argv[2] == '/' || *argv[2] == '%') && b == 0))
 	/*if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))*/
-		printf("error\n"), exit(100);
+		printf("Error\n"), exit(100);
 	printf("%d\n", op_f(a, b));
 	return (0);
 }
