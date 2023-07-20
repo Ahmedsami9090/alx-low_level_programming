e9fb3914c10108e7bd61db2a5377130caf91fa32
Ahmed Sami <ahmadsami990990@gmail.com>
2023-07-20 13:51:34 -0700

    dsa

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,14 +9,14 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int i = n;
+	int i = 0;
 	va_list arg;
 	int sum = 0;
 
 	if (n == 0)
 		return (0);
 	va_start(arg, n);
-	while (i--)
+	for (; i < n; i++)
 		sum = sum + va_arg(arg, int);
 	va_end(arg);
 	return (sum);
